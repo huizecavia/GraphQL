7904da0e9a9973fa49edf73b164e048734f3182a
matthijs <bomaltennishal@outlook.com>
2023-07-09 16:54:00 +0200

    voorbeeld overnemen

---
+++ src/main/java/nl/matthijs/graphql/demo/GraphlNoDBApplication.java
@@ -0,0 +1,13 @@
+package nl.matthijs.graphql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphlNoDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphlNoDBApplication.class, args);
+	}
+
+}
